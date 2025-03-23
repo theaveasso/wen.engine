bf576417f90c92e1126605f4e30b0b3fa608f91f
t7o <theaveas.so@gmail.com>
2025-03-23 15:39:15 +0700

    sandbox test

--- sandbox/source/sandbox.cpp
+++ sandbox/source/sandbox.cpp
@@ -1,10 +1,40 @@
 #include <wen/wen.hpp>
 
-using namespace wen;
-
+namespace sandbox {
 enum struct TileKind { TURRET, PATH, OTHER };
 
+template <typename T> class Grid {};
+
+using namespace wen::component;
+struct Game {
+  flecs::entity window;
+  flecs::entity level;
+
+  transform::Vec2 center = {};
+  float           size   = 0;
+};
+
+struct Level {
+  Level() = default;
+  Level(Grid<TileKind>* arg_map, transform::Vec2 arg_spawn)
+      : spawning_point(arg_spawn), map(arg_map) {}
+
+  transform::Vec2 spawning_point = {};
+  Grid<TileKind>* map            = nullptr;
+};
+
+struct Enemy {};
+
+struct Laser {};
+
+struct Particle {};
+
 namespace prefabs {}
+} // namespace sandbox
+
+using namespace wen;
+
+struct game_level {};
 
 void init_game(flecs::world& world);
 void init_component(flecs::world& world);
@@ -12,6 +42,7 @@ void init_component(flecs::world& world);
 int main(int /* argc */, char** /* argv */) {
   flecs::world world;
 
+  world.import <wen::component::Transform>();
   world.import <wen::system::Engine>();
 
   init_component(world);
@@ -28,7 +59,24 @@ void init_component(flecs::world& world) {
       .emplace<component::WindowComponent>(600, 800)
       .emplace<component::SDL_RendererComponent>()
       .emplace<component::ImGui_GFXComponent>();
+
+  world.component<sandbox::Game>()
+      .member("window", &sandbox::Game::window)
+      .member("level", &sandbox::Game::level)
+      .member("center", &sandbox::Game::center)
+      .member("size", &sandbox::Game::size);
+
+  world.component<sandbox::Enemy>();
+
+  world.component<sandbox::Laser>();
+
+  world.component<sandbox::Particle>();
 }
 
 void init_game(flecs::world& world) {
+  auto& game = world.ensure<sandbox::Game>();
+  //  game.center = {};
+  //  game.size =
+
+  world.script().filename("").run();
 }
--- wengine/include/wen/wen.hpp
+++ wengine/include/wen/wen.hpp
@@ -4,16 +4,17 @@
 #include <flecs.h>
 
 #include "wen/component/gui.hpp"
-#include "wen/component/input.hpp"
-#include "wen/component/window.hpp"
-#include "wen/component/renderer.hpp"
 #include "wen/component/imgui_gfx.hpp"
+#include "wen/component/input.hpp"
+#include "wen/component/renderer.hpp"
+#include "wen/component/transform.hpp"
+#include "wen/component/window.hpp"
 
 #include "wen/component/input.hpp"
 #include "wen/system/engine.hpp"
-#include "wen/system/window.hpp"
-#include "wen/system/renderer.hpp"
 #include "wen/system/imgui_gfx.hpp"
+#include "wen/system/renderer.hpp"
+#include "wen/system/window.hpp"
 
 #ifdef __cplusplus
 extern "C" {
